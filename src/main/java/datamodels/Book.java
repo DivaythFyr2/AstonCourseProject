a4181caaf5642248f8846c329cdcc5ab75223f55
Vlad Afanasev <vlad-vladik-afanasev@mail.ru>
2024-12-22 15:18:09 +0300

    Created custom classes

---
+++ src/main/java/datamodels/Book.java
@@ -0,0 +1,59 @@
+package datamodels;
+
+import java.util.Comparator;
+
+public class Book {
+    private String title;
+    private String author;
+    private int pageCount;
+
+    public Book(BookBuilder bookBuilder) {
+        this.title = bookBuilder.title;
+        this.author = bookBuilder.author;
+        this.pageCount = bookBuilder.pageCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    static class BookBuilder {
+        private String title;
+        private String author;
+        private int pageCount;
+
+        public BookBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BookBuilder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public BookBuilder pageCount(int pageCount) {
+            this.pageCount = pageCount;
+            return this;
+        }
+
+        public Book build(BookBuilder bookBuilder) {
+            return new Book(this);
+        }
+    }
+    public static class BookPageCountComparator implements Comparator<Book> {
+
+        @Override
+        public int compare(Book book1, Book book2) {
+            return Integer.compare(book1.getPageCount(), book2.getPageCount());
+        }
+    }
+}
---
+++ src/main/java/datamodels/RootVegetable.java
@@ -0,0 +1,60 @@
+package datamodels;
+
+import java.util.Comparator;
+
+public class RootVegetable {
+    private String type;
+    private double weight;
+    private String color;
+
+    public RootVegetable(RootVegetableBuilder rootVegetableBuilder) {
+        this.type = rootVegetableBuilder.type;
+        this.weight = rootVegetableBuilder.weight;
+        this.color = rootVegetableBuilder.color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    static class RootVegetableBuilder {
+        private String type;
+        private double weight;
+        private String color;
+
+        public RootVegetableBuilder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public RootVegetableBuilder weight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public RootVegetableBuilder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public RootVegetable build(RootVegetableBuilder rootVegetableBuilder) {
+            return new RootVegetable(this);
+        }
+    }
+
+    public static class RootVegetableWeightComparator implements Comparator<RootVegetable> {
+
+        @Override
+        public int compare(RootVegetable rootVegetable1, RootVegetable rootVegetable2) {
+            return Double.compare(rootVegetable1.getWeight(), rootVegetable2.getWeight());
+        }
+    }
+}
