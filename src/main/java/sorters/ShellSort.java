13f12af21990f8f148d6df61f815052f0ec3f083
Vlad Afanasev <vlad-vladik-afanasev@mail.ru>
2024-12-22 20:37:14 +0300

    An attempt to implement universal sorting

--- src/main/java/sorters/ShellSort.java
+++ src/main/java/sorters/ShellSort.java
@@ -1,17 +1,18 @@
 package sorters;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
-public class ShellSort<T extends Comparable<T>> implements SortStrategy<T> {
-
+public class ShellSort<T> implements SortStrategy<T> {
     @Override
-    public void sort(List<T> items) {
-        int count = items.size();
-        for (int gap = count / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < count; i++) {
+    public void sort(List<T> items, Comparator<T> comparator) {
+        int n = items.size();
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
                 T temp = items.get(i);
                 int j;
-                for (j = i; j >= gap && items.get(j - gap).compareTo(temp) > 0; j -= gap) {
+                for (j = i; j >= gap && comparator.compare(items.get(j - gap), temp) > 0; j -= gap) {
                     items.set(j, items.get(j - gap));
                 }
                 items.set(j, temp);
