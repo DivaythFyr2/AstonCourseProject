1e5399708553a825e30377767910ff366ac81b9b
noir1915 <astronaut95@list.ru>
2024-12-26 21:29:53 +0400

    CustomSort commit

--- src/main/java/sorters/CustomSort.java
+++ src/main/java/sorters/CustomSort.java
@@ -5,54 +5,46 @@ import datamodels.Car;
 import datamodels.RootVegetable;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class CustomSort<T> {
 
+    //  определение четных и нечетных элементов и добавление их в список
     public void sort(List<T> items) {
-        List<T> evenValues = new ArrayList<>();
+        ArrayList<T> evenValues = new ArrayList<>();
+        // Заполнение списка четными элементами
         for (T item : items) {
-            double value = 0;
-
-            if (item instanceof Car) {
-                value = ((Car) item).getPower();
-            } else if (item instanceof Book) {
-                value = ((Book) item).getPageCount();
-            } else if (item instanceof RootVegetable) {
-                value = ((RootVegetable) item).getWeight();
-            }
-
-            if (value % 2 == 0) {
-                evenValues.add(item);
-            } else {
-                evenValues.add(null);
-            }
-        }
-
-        List<Object> sortedEvenValues = new ArrayList<>();
-        for (Object item : items) {
             if (item instanceof Car && ((Car) item).getPower() % 2 == 0) {
-                sortedEvenValues.add(item);
+                evenValues.add(item);
             } else if (item instanceof Book && ((Book) item).getPageCount() % 2 == 0) {
-                sortedEvenValues.add(item);
+                evenValues.add(item);
             } else if (item instanceof RootVegetable && ((RootVegetable) item).getWeight() % 2 == 0) {
-                sortedEvenValues.add(item);
+                evenValues.add(item);
             }
         }
 
-        sortedEvenValues.sort(Comparator.comparingInt(o -> {
-            if (o instanceof Car) return (int) ((Car) o).getPower();
-            if (o instanceof Book) return ((Book) o).getPageCount();
-            if (o instanceof RootVegetable) return (int) ((RootVegetable) o).getWeight();
+        //Сортировка четных элементов
+        evenValues.sort((o1, o2) -> {
+            if (o1 instanceof Car && o2 instanceof Car) {
+                return Integer.compare(((Car) o1).getPower(), ((Car) o2).getPower());
+            } else if (o1 instanceof Book && o2 instanceof Book) {
+                return Integer.compare(((Book) o1).getPageCount(), ((Book) o2).getPageCount());
+            } else if (o1 instanceof RootVegetable && o2 instanceof RootVegetable) {
+                return Double.compare(((RootVegetable) o1).getWeight(), ((RootVegetable) o2).getWeight());
+            }
             return Integer.MAX_VALUE;
-        }));
+        });
 
+        //Индекс для отслеживания четных значений
         int evenIndex = 0;
 
+        // заполнение списка, оставляя нечетные значения на своих местах
         for (int i = 0; i < items.size(); i++) {
-            if (evenValues.get(i) != null) {
-                items.set(i, (T) sortedEvenValues.get(evenIndex++));
+            T item = items.get(i);
+            if ((item instanceof Car && ((Car) item).getPower() % 2 == 0) ||
+                    (item instanceof Book && ((Book) item).getPageCount() % 2 == 0) ||
+                    (item instanceof RootVegetable && ((RootVegetable) item).getWeight() % 2 == 0)) {
+                items.set(i, evenValues.get(evenIndex++));  // Замена элемента в указанной позиции в этом списке указанным элементом
             }
         }
     }
