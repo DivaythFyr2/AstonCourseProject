eba348fd9186a176f62e287686603dc3a84866f3
Vlad Afanasev <vlad-vladik-afanasev@mail.ru>
2024-12-26 18:33:31 +0300

    Added filewriter

---
+++ src/main/java/filewriter/FileWriterUtil.java
@@ -0,0 +1,33 @@
+package filewriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class FileWriterUtil {
+    private static final String ERROR = "Ошибка записи в файл: ";
+    private static final String SUCCESS = "Запись успешно завершена";
+
+    public static <T> void writeToFile(String filePath, List<T> data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            for (T item : data) {
+                writer.write(item.toString());
+                writer.newLine();
+            }
+            System.out.println(SUCCESS);
+        } catch (IOException e) {
+            System.out.println(ERROR + e.getMessage());
+        }
+    }
+
+    private static <T> void writeSingleObjectToFile(String filePath, T object) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(object.toString());
+            writer.newLine();
+            System.out.println(SUCCESS);
+        } catch (IOException e) {
+            System.out.println(ERROR + e.getMessage());
+        }
+    }
+}
